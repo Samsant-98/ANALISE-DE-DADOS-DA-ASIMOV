61079b5f422ffa7c207fcb6241f286833c5708d9
SamuelSantanaesilva <114364164+SamuelSantanaesilva@users.noreply.github.com>
2025-01-10 14:24:57 -0300

    adicionei a analise de adados

---
+++ asimov.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+pd.set_option('display,max_colurns'none)
+df_viagens = pd.read_xls('viagens.xls')
+
+df_viagens{{}}
+
+* muda o valor da panilha de vigula para ponto*
+
+df_viagens{} str.replace{'.','.'}
+
+* calcula* 
+df_viagens{}+df_viagens{} 
+
+* calcula a média*
+sdf_viagens{}+df_viagens{}/pela quantidade de viagens
